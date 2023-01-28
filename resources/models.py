b153a6a95cf684c285b75ffaee0bfdd956582eef
Gibril1 <gibrilissakaiddrisu@gmail.com>
2023-01-28 11:03:52 -0800

    blog model

---
+++ resources/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from users.models import User
+
+# Create your models here.
+class Blog(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    content = models.CharField(max_length=1000)
+
+
+    def __str__(self) -> str:
+        return self.content
